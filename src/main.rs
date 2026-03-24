8261ee9dab2d7d670f6d6a08668186a8289d30df
jannikac <jannikac@mailbox.org>
2026-03-24 13:28:52 +0100

    wrap AppState in rc for sharing. add basic validation

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,11 @@
 slint::include_modules!();
+use std::str::FromStr;
+
 use anyhow::{Context, Result};
+use bytesize::ByteSize;
 use mtp_filler::AppState;
+use std::cell::RefCell;
+use std::rc::Rc;
 
 #[cfg(windows)]
 mod windows;
@@ -11,7 +16,7 @@ mod unix;
 mod shared;
 
 fn main() -> Result<()> {
-    let mut app_state = AppState::new();
+    let app_state = Rc::new(RefCell::new(AppState::new()));
     let main_window = MainWindow::new()?;
     let main_window_weak = main_window.as_weak();
     let my_vec = vec![
@@ -20,8 +25,46 @@ fn main() -> Result<()> {
         slint::SharedString::from("three"),
     ];
 
+    let handle2 = main_window_weak.clone();
+    let app_state_for_write = app_state.clone();
+    main_window.on_write_clicked(move |space_to_leave, selected_index, keep_local| {
+        let handle2 = handle2.upgrade().unwrap();
+        let app_state = app_state_for_write.borrow();
+
+        handle2.set_space_to_leave_error("".into());
+
+        let space_to_leave = match ByteSize::from_str(&space_to_leave) {
+            Ok(v) => v,
+            Err(e) => {
+                handle2.set_space_to_leave_error(slint::SharedString::from(e));
+                return ();
+            }
+        };
+
+        let device_index: usize = match selected_index.try_into() {
+            Ok(v) => v,
+            Err(_) => {
+                handle2
+                    .set_select_device_error(slint::SharedString::from("Please select a device"));
+                return ();
+            }
+        };
+
+        let selected_option = match &app_state.select_options.get(device_index) {
+            Some(v) => *v,
+            None => {
+                handle2
+                    .set_select_device_error(slint::SharedString::from("Invalid device selection"));
+                return ();
+            }
+        };
+        dbg!(space_to_leave, selected_index, keep_local);
+    });
+
     main_window.set_combo_options(slint::ModelRc::new(slint::VecModel::from(my_vec)));
+    let app_state_for_refresh = app_state.clone();
     main_window.on_refresh_clicked(move || {
+        let mut app_state = app_state_for_refresh.borrow_mut();
         let handle2 = main_window_weak.upgrade().unwrap();
         let refresh_result = app_state.refresh();
 
@@ -34,7 +77,7 @@ fn main() -> Result<()> {
                     .collect::<Vec<_>>();
                 handle2.set_combo_options(slint::ModelRc::new(slint::VecModel::from(options)));
             }
-            Err(e) => handle2.set_select_device_text(slint::SharedString::from(e.to_string())),
+            Err(e) => handle2.set_select_device_error(slint::SharedString::from(e.to_string())),
         }
     });
 
