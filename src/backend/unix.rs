ed5fe51c8379460defed7a814eafa31521c7cfde
jannikac <jannikac@mailbox.org>
2026-03-25 18:17:31 +0100

    fix cpi output

--- src/backend/unix.rs
+++ src/backend/unix.rs
@@ -130,6 +130,9 @@ impl SelectOption {
         let s = format!("{}\n{}", self.device, self.storage);
         SharedString::from(s)
     }
+    pub fn to_string(&self) -> String {
+        format!("{} - {}", self.device, self.storage)
+    }
 }
 
 pub struct DeviceState {
